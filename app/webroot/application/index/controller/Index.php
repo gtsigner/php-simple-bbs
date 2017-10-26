d54f6ae074703f9dfce1dc21cf2fe9479c45f8a7
zhaojunlike <zhaojunlike@gmail.com>
2017-10-26 09:52:31 +0800

    前台状态

--- app/webroot/application/index/controller/Index.php
+++ app/webroot/application/index/controller/Index.php
@@ -25,7 +25,7 @@ class Index extends Auth
         }
         $data = model('bbs_post')
             ->where($map)
-            ->wherew(['status' => 1gi])
+            ->where(['status' => 1])
             ->withCount("comments")
             ->order('position DESC,create_time DESC,sort ASC')
             ->paginate($this->page_limit);
